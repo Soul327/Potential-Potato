ea66708da74ad896fdc874403fc64bb5a24ef0d3
souls <souls@SurfaceLaptop4>
2021-12-02 20:59:46 -0600

    Added comments in TestState

--- Space/src/Main/TestSpeed.java
+++ Space/src/Main/TestSpeed.java
@@ -1,47 +1,17 @@
 package Main;
 
+import java.util.ArrayList;
+
 public class TestSpeed {
 	public static void main(String args[]) {
 		long[][] times = new long[2][2];
-		for(int z=0;z<100_000_000;z++) {
-			long startTime;
-			//Create vars to pass in, must be the same for both test cases
-			int[] a = new int[4];
-			int[] b = new int[3];
-			for(int x=0;x<a.length;x++) a[x] = (int)(Math.random()*255);
-			for(int x=0;x<b.length;x++) b[x] = (int)(Math.random()*255);
-			
-			startTime = System.nanoTime();
-			add1(a,b);
-			times[0][0] += System.nanoTime() - startTime;
-			times[0][1] ++;
-			
-			startTime = System.nanoTime();
-			add2(a,b);
-			times[1][0] += System.nanoTime() - startTime;
-			times[1][1] ++;
-			
-			if(z%100_000==0) {
-				System.out.println("add1() "+(times[0][0]/times[0][1]));
-				System.out.println("add2() "+(times[1][0]/times[1][1]));
-				System.out.println();
-			}
-		}
+	}
+	public static void store1(int amt) {
+		ArrayList<Double> list = new ArrayList<Double>();
+		for(int z=0;z<amt;z++)
+			list.add(Math.random() );
 		
-	}
-	public static int[] add1(int[] a, int[] b){
-		if(b[0]==0 && b[1]==0 && b[2]==0) return a;
-		for(int z=0;z<3;z++)
-			a[z] += b[z];
-		a[3] += 1;
-		return a;
-	}
-	public static int[] add2(int[] a, int[] b){
-		if(b[0]!=0 && b[1]!=0 && b[2]!=0) {
-			for(int z=0;z<3;z++)
-				a[z] += b[z];
-			a[3] += 1;
-		}
-		return a;
+		for(int z=0;z<amt/2;z++)
+			list.get( (int)(Math.random()*amt));
 	}
 }
--- Space/src/Main/TestState.java
+++ Space/src/Main/TestState.java
@@ -12,6 +12,7 @@ import Misc.MatrixManipulation;
 import static Misc.SDL.*;
 
 public class TestState {
+	// This class provites a simple way to store multiple variables in one
 	class Point {
 		int x,y;
 		public Point(int x, int y) {
@@ -20,17 +21,18 @@ public class TestState {
 		}
 	}
 	
-	public TestState() {
-		
-	}
 	
 	BufferedImage img;
-	double randA = 10;
-	double[][][] raw;
+	double randA = 10;// Variation amount of each pixel, each pixel will vary in rgb color by this amount in the positive and negitive direction
+	// This is a double due to rounding having a large effect on the slight varations in the colors
+	double[][][] raw;// This is the raw image "file", it is a global varible so I don't have to pass it in to every function
+	
+	// Generates a new image are stores it in img and raw
 	public void gen() {
-		
+		// Reset/initizes raw img file
 		raw = new double[Launcher.window.width][Launcher.window.height][3];
 		
+		// Initilizes the raw image with points to start from set to random color and location
 		for(int z=0;z<100;z++) {
 			int x = (int)(Math.random()*raw.length);
 			int y = (int)(Math.random()*raw[0].length);
@@ -39,40 +41,32 @@ public class TestState {
 			raw[x][y][2] = Math.random()*255;
 		}
 		
-		//Basic system
-//		for(int x=0;x<raw.length;x++)
-//			for(int y=0;y<raw[x].length;y++)
-//				if(raw[x][y][0] == 0 && raw[x][y][1] == 0 && raw[x][y][2] == 0) {
-//					double[] col = getAdvColorNear(raw, x, y);
-//					raw[x][y][0] = Math.random()*(randA*2)-randA+col[0];
-//					raw[x][y][1] = Math.random()*(randA*2)-randA+col[1];
-//					raw[x][y][2] = Math.random()*(randA*2)-randA+col[2];
-//				}
-		
-		//Find and generate
-		ArrayList<Point> listOfPixels = new ArrayList<Point>();
-		ArrayList<Point> listOfPixelsDone = new ArrayList<Point>();
+		ArrayList<Point> listOfPixels = new ArrayList<Point>();// List of pixel that are going to be rendered 
+		ArrayList<Point> listOfPixelsDone = new ArrayList<Point>();// List of pixels that have previously been rendered
 		println("Adding pixels to list.");
-		//Scan to find any points and add them to the list
+		//Scan to find any points and add them to the list, this only needs to run once
 		for(int x=0;x<raw.length;x++)
 			for(int y=0;y<raw[x].length;y++) {
+				// Check if x,y is part of prevous pixels and ignore them. This is no longer need as this only runs once at the beguining when listOfPixelsDone is empty
 				boolean check = true;
 				for(Point p:listOfPixelsDone)
-					if(p.x == x && p.y == y) {
+					if(p.x == x && p.y == y) {// This is never true?
 						check = false;
 						break;
 					}
+				// Add found point to list
 				if(check && (raw[x][y][0] != 0 || raw[x][y][1] != 0 || raw[x][y][2] != 0))
 					listOfPixels.add( new Point(x, y) );
 			}
+		
 		while(!isDone()) {
 			println("Generating new pixels.");
 			//Randomize list and 
 			for(int z=0;z<listOfPixels.size();z++) {
-				if(Math.random() < .5) continue;
-				Point p = listOfPixels.remove(z--);
-				listOfPixelsDone.add( p );
-//				println( "X:"+p.x+" Y:"+p.y );
+				if(Math.random() < .5) continue;// Randomly skips pixels to not make everything diamonds
+				Point p = listOfPixels.remove(z--); listOfPixelsDone.add( p ); // Moves the pixel to the listOfPixelsDone
+				
+				// Generate pixels arround selected pixel, then add them to the listOfPixels
 				if(p.x > 0 && c(raw[p.x-1][p.y])) listOfPixels.add( generatePixel(p.x-1,p.y) );
 				if(p.y > 0 && c(raw[p.x][p.y-1])) listOfPixels.add( generatePixel(p.x,p.y-1) );
 				if(p.x < raw.length - 1 && c(raw[p.x+1][p.y])) listOfPixels.add( generatePixel(p.x+1,p.y) );
@@ -87,6 +81,8 @@ public class TestState {
 					setColor(img, x, y, raw[x][y][0], raw[x][y][1], raw[x][y][2]);
 		}
 	}
+	
+	// Check if the raw image has any cases where the colors are zero
 	public boolean isDone() {
 		for(int x=0;x<raw.length;x++)
 			for(int y=0;y<raw[x].length;y++)
@@ -94,6 +90,8 @@ public class TestState {
 					return false;
 		return true;
 	}
+	
+	// Creates a color and sets it to x,y then returns the Point of the pixel to save myself time on other code
 	public Point generatePixel(int x, int y) {
 		double[] col = getAdvColorNear(raw, x, y);
 		raw[x][y][0] = Math.random()*(randA*2)-randA+col[0];
@@ -101,9 +99,13 @@ public class TestState {
 		raw[x][y][2] = Math.random()*(randA*2)-randA+col[2];
 		return new Point(x,y);
 	}
+	
+	// Checks to see if the color list provided is not zero 
 	public boolean c(double[] list) {
 		return list[0]==0 && list[1]==0 && list[2]==0;
 	}
+	
+	// Calculates and returns the advColor near the pixel chosen in raw then returns it in {r,g,b}
 	public double[] getAdvColorNear(double[][][] raw, int x, int y) {
 		double[] re = new double[4];
 		if(x>0) re = add(re, raw[x-1][y]);
@@ -113,6 +115,8 @@ public class TestState {
 		for(int z=0;re[3] > 0 && z<re.length;z++) re[z] /= re[3];
 		return re;
 	}
+	
+	// Adds the color array (b) to (a) by adding the first three then incrementing the fourth var in (a)
 	public static double[] add(double[] a, double[] b){
 		if(b[0]==0 && b[1]==0 && b[2]==0) return a;
 			a[0] += b[0];
@@ -123,14 +127,7 @@ public class TestState {
 		return a;
 	}
 	
-	public int[] getColor(BufferedImage img, int x, int y) {
-		int[] re = new int[3];
-		int clr = img.getRGB(x, y);
-		re[0] = (clr & 0x00ff0000) >> 16;
-		re[1] = (clr & 0x0000ff00) >> 8;
-		re[2] = clr & 0x000000ff;
-		return re;
-	}
+	// Sets the color of a pixel in a bufferedImage
 	public void setColor(BufferedImage img, int x, int y, double red, double green, double blue) {
 //		System.out.printf("%f  %f  %f%n",red,green,blue);
 		int r = (int) Mat.inRange(red, 0, 255);
@@ -139,7 +136,10 @@ public class TestState {
 		int col = (r << 16) | (g << 8) | b;
 		img.setRGB(x, y, col);
 	}
+	
+	//Tick and Render, called from Launcher
 	public void tar(Graphics g) {
+		// When space is pressed and released a new thread will start the generation. There is no check to see if the thread is running, yes this is an issue
 		if(KeyManager.keyRelease(KeyEvent.VK_SPACE)) {
 			(new Thread() {
 				public void run() {
@@ -147,6 +147,8 @@ public class TestState {
 				}
 			}).start();
 		}
+		
+		// Draws image to the screen in native resolution, if img is null this is skiped without error
 		g.drawImage( img );
 	}
 }
